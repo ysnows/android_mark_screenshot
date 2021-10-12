7258f13ff5d4809f34dba9a594c2138483c909a9
ysnows <ysnows@163.com>
2021-10-12 10:00:18 +0800

    1. 颜色选择器

--- app/src/main/java/com/windhike/tuto/easytouch/view/ColorHolder.java
+++ app/src/main/java/com/windhike/tuto/easytouch/view/ColorHolder.java
@@ -42,7 +42,8 @@ public class ColorHolder extends BaseTouchViewHolder{
     @Override
     public WindowManager.LayoutParams getViewLayoutParams() {
         WindowManager.LayoutParams windowLayoutParams = new WindowManager.LayoutParams();
-        windowLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+//        windowLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+        windowLayoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
         windowLayoutParams.format = PixelFormat.RGBA_8888;
         windowLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
 
